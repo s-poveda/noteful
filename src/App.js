4fb950cc461f88fdd27b469200b4aa77323baaeb
s-poveda <spoveda545@gmail.com>
2020-10-06 12:23:26 -0400

    wip basic outline

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
+class App extends Component {
+	static defaultProps = {
+		notes: [],
+		folders: [],
+	}
+
+	state = {
+		notes: this.props.notes,
+		folders: this.props.folders,
+		selectedFolderId: null
+	}
+
+	setSelectedFolderId = (id) => {
+		this.setState({ selectedFolderId: id });
+	}
+
+	notesToBeDisplayed = () => {
+		return this.state.selectedFolderId? this.state.notes.filter(note => note.folderId === this.state.selectedFolderId): this.state.notes;
+	}
+
+  render () {
+		return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+			<Route exact path="/"
+			// {component={FolderMenu}}
+			 	render={ () =>  <div> <FolderMenu /> <MainDisplay notesToBeDisplayed={this.notesToBeDisplayed}/> </div>} />
+			<Route exact path='/folder/:folder-id'
+				render={ () =>  <div> <FolderMenu /> <MainDisplay notesToBeDisplayed={this.notesToBeDisplayed}/> </div>} />
+			<Route exact path='/note/:note-id' component={singleNoteDisplay}/>
     </div>
-  );
+  	);
+	}
 }
 
 export default App;

972db2b011b2a9a00fdbf16e7d36f7818ce562c9
Metty Schroeder <mettyj.schroeder@gmail.com>
2020-10-08 15:41:09 -0400

    minor changes

--- src/AddFolder/AddFolder.js
+++ src/AddFolder/AddFolder.js
@@ -19,15 +19,14 @@ class AddFolder extends Component {
       id='add-folder-form'
       className='Noteful-form'
       action='#'
-      onSubmit={e => {e.preventDefault(); addFolder()}}
+      onSubmit={e => {e.preventDefault();  addFolder(this.state.title)}}
     >
       <label htmlFor='folder-name-input'>New Folder Name:</label>
-      <input type='text' className='field' id='folder-name-input' value={this.state.title.value} onChange={e => {
+      <input type='text' name='folder-title' className='field' id='folder-name-input' value={this.state.title.value} onChange={e => {
         console.log(e.target.value);
         this.setState({
           title: {value: e.target.value}
         })
-
       }}/>
       <button type='submit' className='buttons'>Create Folder</button>
     </form>
